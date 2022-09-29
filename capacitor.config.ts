83be327b3f3a371b483ea9835658e6f5f6c981f8
Ben Finnegan <bfinnegan3@huskers.unl.edu>
2022-09-29 15:54:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helloWorld',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
